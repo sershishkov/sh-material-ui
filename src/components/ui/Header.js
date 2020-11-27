d611280c763afb54cafdfd0fc03343bfd8bf914f
sershishkov <ser.shishkov@gmail.com>
2020-11-27 21:05:15 +0200

    02-012

---
+++ src/components/ui/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+function Header() {
+  return (
+    <AppBar>
+      <Toolbar>sh-header</Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
