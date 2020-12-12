15aba713d6cb02cf53508262f5f9344ef55388e0
sershishkov <ser.shishkov@gmail.com>
2020-12-12 20:38:02 +0200

    6-85

--- src/components/customSoftware/CustomSoftware.js
+++ src/components/customSoftware/CustomSoftware.js
@@ -4,15 +4,39 @@ import { Link } from 'react-router-dom';
 
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import backArrow from '../../assets/backArrow.svg';
 import forwardArrow from '../../assets/forwardArrow.svg';
+import lightbulb from '../../assets/bulb.svg';
+import cash from '../../assets/cash.svg';
+import stopwatch from '../../assets/stopwatch.svg';
 
-const useStyles = makeStyles((theme) => ({}));
+import documentsAnimation from '../../animations/documentsAnimation/data.js';
+import scaleAnimation from '../../animations/scaleAnimation/data.json';
+
+const useStyles = makeStyles((theme) => ({
+  mainContainer: {
+    paddingLeft: '5em',
+    paddingRight: '5em',
+    paddingTop: '2em',
+    paddingBottom: '10em',
+  },
+  heading: {
+    maxWidth: '40em',
+  },
+  arrowContainer: {
+    marginTop: '0.5em',
+  },
+  iconButton: {
+    backgroundColor: 'transparent',
+  },
+  itemContainer: {
+    maxWidth: '40em',
+  },
+}));
 
 const CustomSoftware = (props) => {
   const classes = useStyles();
@@ -20,16 +44,46 @@ const CustomSoftware = (props) => {
   const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
   const matchesXS = useMediaQuery(theme.breakpoints.down('xs'));
 
+  const documentsOptions = {
+    loop: true,
+    autoplay: false,
+    animationData: documentsAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
+  const scaleOptions = {
+    loop: true,
+    autoplay: false,
+    animationData: scaleAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
   return (
-    <Grid container direction='column'>
+    <Grid container direction='column' className={classes.mainContainer}>
       <Grid item container direction='row'>
-        <Grid item>
-          <IconButton>
+        <Grid
+          item
+          className={classes.arrowContainer}
+          style={{
+            marginRight: '1em',
+            marginLeft: '-3.5em',
+          }}
+        >
+          <IconButton
+            className={classes.iconButton}
+            component={Link}
+            to='/services'
+            onClick={() => props.setSelectedIndex(0)}
+          >
             <img src={backArrow} alt='Back to Services Page' />
           </IconButton>
         </Grid>
 
-        <Grid item direction='column'>
+        <Grid item container direction='column' className={classes.heading}>
           <Grid item>
             <Typography variant='h2'>Custom Software Development</Typography>
           </Grid>
@@ -57,13 +111,132 @@ const CustomSoftware = (props) => {
             </Typography>
           </Grid>
         </Grid>
-        <Grid item>
-          <IconButton>
+
+        <Grid item className={classes.arrowContainer}>
+          <IconButton
+            className={classes.iconButton}
+            component={Link}
+            to='/mobileapps'
+            onClick={() => props.setSelectedIndex(2)}
+          >
             <img
               src={forwardArrow}
               alt='Forward to iOS/Android App Delepment Page'
             />
           </IconButton>
+        </Grid>
+      </Grid>
+
+      <Grid
+        item
+        container
+        direction='row'
+        justify='center'
+        style={{ marginTop: '15em', marginBottom: '20em' }}
+      >
+        <Grid
+          item
+          container
+          direction='column'
+          md
+          style={{ maxWidth: '40em' }}
+          alignItems='center'
+        >
+          <Grid item>
+            <Typography variant='h4'>Save Energy</Typography>
+          </Grid>
+          <Grid item>
+            <img src={lightbulb} alt='lightbulb' />
+          </Grid>
+        </Grid>
+
+        <Grid
+          item
+          container
+          direction='column'
+          md
+          style={{ maxWidth: '40em' }}
+          alignItems='center'
+        >
+          <Grid item>
+            <Typography variant='h4'>Save Time</Typography>
+          </Grid>
+          <Grid item>
+            <img src={stopwatch} alt='stopwatch' />
+          </Grid>
+        </Grid>
+
+        <Grid
+          item
+          container
+          direction='column'
+          md
+          style={{ maxWidth: '40em' }}
+          alignItems='center'
+        >
+          <Grid item>
+            <Typography variant='h4'>Save Money</Typography>
+          </Grid>
+          <Grid item>
+            <img src={cash} alt='stopwatch' />
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid item container direction='row'>
+        <Grid item container className={classes.itemContainer}>
+          <Grid item container direction='column' md>
+            <Grid item>
+              <Typography variant='h4'> Digital Documents & Data</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant='body1' paragraph>
+                Reduce Errors. Reduce Waste. Reduce Costs.
+              </Typography>
+              <Typography variant='body1' paragraph>
+                Billions are spent annually on the purchasing, printing, and
+                distribution of paper. On top of the massive environmental
+                impact this has, it causes harm to your bottom line as well.
+              </Typography>
+              <Typography variant='body1' paragraph>
+                By utilizing digital forms and documents you can remove these
+                obsolete expenses, accelerate your communication, and help the
+                Earth.
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item md>
+            <Lottie
+              options={documentsOptions}
+              isStopped={true}
+              style={{ maxHeight: 325, maxWidth: 275, minHeight: 275 }}
+            />
+          </Grid>
+        </Grid>
+
+        <Grid item container className={classes.itemContainer}>
+          <Grid item md>
+            <Lottie
+              options={scaleOptions}
+              isStopped={true}
+              style={{ maxHeight: 260, maxWidth: 280 }}
+            />
+          </Grid>
+          <Grid item container direction='column' md>
+            <Grid item>
+              <Typography variant='h4' align='right'>
+                {' '}
+                Scale
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant='body1' paragraph align='right'>
+                Whether you’re a large brand, just getting started, or taking
+                off right now, our application architecture ensures pain-free
+                growth and reliability.
+              </Typography>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
