cebe7a3a461cd104a9f4cf5eab030929f558a19f
sershishkov <ser.shishkov@gmail.com>
2020-12-26 19:03:15 +0200

    08-finished

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,44 @@
 const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
 const admin = require('firebase-admin');
-const config = functions.config();
+const cors = require('cors')({ origin: true });
 
+const config = functions.config();
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// настраиваем емаил и пароль в firebase
+// firebase functions:config:set user.email='noreply.arcdevelopment.com'
+// firebase functions:config:set user.password='my-password'
+
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: config.user.email,
+    pass: config.user.password,
+  },
+});
+
+let mailOptions = {
+  from: 'Arc development',
+  to: 'bar@example.com, baz@example.com',
+  subject: 'Hello ✔',
+  text: 'Hello world?',
+  html: '<b>Hello world?</b>',
+};
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.sendMail = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    transporter.sendMail(mailOptions, (error) => {
+      if (error) {
+        response.send(error);
+      } else {
+        response.send('Message sent successfuly');
+      }
+    });
+  });
+});
+
+//firebase deploy
