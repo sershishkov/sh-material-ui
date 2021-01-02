d9372e8114fc671a9af5b72fc90788ef92306c5c
sershishkov <ser.shishkov@gmail.com>
2021-01-02 21:01:18 +0200

    10-144

--- src/components/estimate/Estimate.js
+++ src/components/estimate/Estimate.js
@@ -485,6 +485,26 @@ const Estimate = (props) => {
     // console.log(cost);
   };
 
+  const getPlatforms = () => {
+    let newPlatforms = [];
+    const wichPlatform = questions.filter(
+      (question) => question.title === 'Which platforms do you need supported?'
+    );
+    // console.log(wichPlatform);
+
+    if (wichPlatform.length > 0) {
+      wichPlatform.map((question) =>
+        question.options
+          .filter((option) => option.selected)
+          .map((option) => newPlatforms.push(option.title))
+      );
+    }
+
+    setPlatforms(newPlatforms);
+
+    console.log(newPlatforms);
+  };
+
   const onChange = (event) => {
     let valid;
 
@@ -653,6 +673,7 @@ const Estimate = (props) => {
             onClick={() => {
               setOpenDialog(true);
               getTotal();
+              getPlatforms();
             }}
           >
             Get Estimate
@@ -746,7 +767,34 @@ const Estimate = (props) => {
                     </Grid>
                     <Grid item>
                       <Typography variant='body1'>
-                        You want {service}
+                        You want {service}{' '}
+                        {platforms.length > 0
+                          ? `for ${
+                              //if only web application is selected...
+                              platforms.indexOf('Web Application') > -1 &&
+                              platforms.length === 1
+                                ? //then finish sentence here
+                                  'a Web Application.'
+                                : //otherwise, if web application and another platform is selected...
+                                platforms.indexOf('Web Application') > -1 &&
+                                  platforms.length === 2
+                                ? //then finish the sentence here
+                                  `a Web Application and an ${platforms[1]}.`
+                                : //otherwise, if only one platform is selected which isn't web application...
+                                platforms.length === 1
+                                ? //then finish the sentence here
+                                  `an ${platforms[0]}`
+                                : //otherwise, if other two options are selected...
+                                platforms.length === 2
+                                ? //then finish the sentence here
+                                  'an iOS Application and an Android Application.'
+                                : //otherwise if all three are selected...
+                                platforms.length === 3
+                                ? //then finish the sentence here
+                                  'a Web Application, an iOS Application, and an Android Application.'
+                                : null
+                            }`
+                          : null}
                       </Typography>
                     </Grid>
                   </Grid>
